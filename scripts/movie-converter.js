eccc445127e8758d086f30a8d0e4043fac378142
Caleb Ford <caleb.ford.sto@gmail.com>
2024-05-28 14:06:12 -0500

    Implemented main algorithm in main module

--- scripts/movie-converter.js
+++ scripts/movie-converter.js
@@ -1,7 +1,7 @@
 import { movies } from "./movies-data.js"
 
 export const generateMoviesHTML = () => {
-    let movieHTML = "";
+    let movieHTML = '';
 
     for (const movie of movies) {
         movieHTML += `
@@ -9,7 +9,7 @@ export const generateMoviesHTML = () => {
                 <img src="${movie.poster}" alt="${movie.title} poster" class="movie__poster">
                 <div class="movie__details">
                     <h2 class="movie__title">${movie.title}</h2>
-                    <p class="movie__description>${movie.description}</p>
+                    <p class="movie__description">${movie.description}</p>
                 </div>
             </article>
         `;
